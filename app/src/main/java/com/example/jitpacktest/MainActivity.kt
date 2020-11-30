c75c748db1bdbf442fb6eedaf064a0582c9f7b94
Girish Budhwani <girishiitj@gmail.com>
2020-11-30 10:56:45 +0530

    added toast library

--- app/src/main/java/com/example/jitpacktest/MainActivity.kt
+++ app/src/main/java/com/example/jitpacktest/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.jitpacktest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.girish.toast.ToastMessage
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ToastMessage.display(this)
     }
 }
